e0170027acbd07dbed9c26bbe3b2ba846b90a762
jac-lene <jaclenesini@gmail.com>
2022-05-21 13:08:46 -0400

    daily match styling

--- bc-frontend/src/App.js
+++ bc-frontend/src/App.js
@@ -2,14 +2,44 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    
     <header>
+    <nav>Home  Matches   Chart</nav>
       <h1>Horoscope Dating</h1>
+    <nav>My Account   Sign Out</nav>
     </header>
+
+<br></br>
+
+    <div className='left'>
+    <div className='daily-matches'>
+     <div className='daily-match'>
+       <img src="https://thispersondoesnotexist.com/image" alt="match1" className='dm-img'/>
+       <p className='dm-bio'>Bio: sldjald jldsajldjaljlwa dsljaj daiwjlaw jdklakld sjlkadiwam
+         jldjklslslia
+       </p>
+    </div>
+    <div className='daily-match'>
+       <img src="https://thispersondoesnotexist.com/image" alt="match2" className='dm-img' />
+       <p className='dm-bio'>Bio: sldjald jldsajldjaljlwa dsljaj daiwjlaw jdklakld sjlkadiwam
+         jldjklslslia
+       </p>
+    </div>
+    <div className='daily-match'>
+       <img src="https://thispersondoesnotexist.com/image" alt="match3" className='dm-img'/>
+       <p className='dm-bio'>Bio: sldjald jldsajldjaljlwa dsljaj daiwjlaw jdklakld sjlkadiwam
+         jldjklslslia
+       </p>
+    </div>
+
+    </div>
+
 
 
 
     </div>
+    </>
   );
 }
 
