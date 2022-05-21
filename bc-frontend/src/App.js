f591de2a7a4a6db0bb3ca7972e650a2812abcb58
jac-lene <jaclenesini@gmail.com>
2022-05-21 14:12:36 -0400

    responsive started

--- bc-frontend/src/App.js
+++ bc-frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className='body'>
     
     <header>
     <nav>Home  Matches   Chart</nav>
@@ -12,9 +12,8 @@ function App() {
 
 <br></br>
     <div className='lr-cont'>
-   
-
     <div className='left'>
+    <h3 className='comp-title'>Today's Compatibility</h3>
     <div className='daily-matches'>
      <div className='daily-match'>
        <img src="https://thispersondoesnotexist.com/image" alt="match1" className='dm-img'/>
@@ -38,7 +37,7 @@ function App() {
     </div>
 
     <div className='love-horo'>
-    <h4>Love Horoscope</h4>
+    <h4 className='love-title'>Love Horoscope</h4>
         <p>May 21, 2022 - Today's prospects should suit the dynamic, idealistic being that you are, especially right now. There will be some criticism in the air, and you won't miss an opportunity to punctuate your comments with your famous irony. Your viewpoints on corporate rigidity and conformist attitudes are well known, but today your sense of humor prevails and people listen to you with pleasure!</p>
     </div>
 
@@ -46,13 +45,13 @@ function App() {
 
     <div className='right'>
       <div className='daily-horo'>
-        <h4>Daily Horoscope</h4>
+        <h4 className='horo-title'>Daily Horoscope</h4>
         <p>May 21, 2022 - Today's prospects should suit the dynamic, idealistic being that you are, especially right now. There will be some criticism in the air, and you won't miss an opportunity to punctuate your comments with your famous irony. Your viewpoints on corporate rigidity and conformist attitudes are well known, but today your sense of humor prevails and people listen to you with pleasure!</p>
       </div>
     </div>
 
     </div>
-    </>
+    </div>
   );
 }
 
