77c7a660cd4e56863014358ab03ec4325232370a
jac-lene <jaclenesini@gmail.com>
2022-05-23 12:37:22 -0400

    my acct page set up

--- bc-frontend/src/Pages/MyAccount.js
+++ bc-frontend/src/Pages/MyAccount.js
@@ -9,7 +9,10 @@ function MyAccount() {
       <img src="https://thispersondoesnotexist.com/image" alt="profile-img" className='default-img'/>
       </div>
         <div className='acct-header'>
-         <h2>My Account</h2>
+          <div className='myacct-cont'>
+            <h2>My Account</h2>
+            <button>Update Preferences</button>
+          </div>
          <h3>Jaclene Sini, 31 - Brooklyn, New York</h3>
          <h4>Sun: Leo - Moon: Virgo - Rising: Sagittarius</h4>
          <h4>Bio</h4>
@@ -23,7 +26,6 @@ function MyAccount() {
          </p>
          <h4>Photos</h4>
          <div className='photos'>
-         <div className='photo-cont'><img src="https://thispersondoesnotexist.com/image" alt="profile-img" className='profile-img'/></div>
          <div className='photo-cont'><img src="https://thispersondoesnotexist.com/image" alt="profile-img" className='profile-img'/></div>
          <div className='photo-cont'><img src="https://thispersondoesnotexist.com/image" alt="profile-img" className='profile-img'/></div>
          <div className='photo-cont'><img src="https://thispersondoesnotexist.com/image" alt="profile-img" className='profile-img'/></div>
