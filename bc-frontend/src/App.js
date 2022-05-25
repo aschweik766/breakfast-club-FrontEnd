155166b10b67cbd2420df67400a362165020c294
Charnaye Grier <charnayegrier@gmail.com>
2022-05-24 22:32:20 -0400

    merging changes properly with my commits

--- bc-frontend/src/App.js
+++ bc-frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from './Pages/Home';
