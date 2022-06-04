e175649dbb82fed4d0b1de617422f89257aae8ca
Charnaye Grier <charnayegrier@gmail.com>
2022-06-04 03:48:32 -0400

    changed images for home page and changed Users page

--- bc-frontend/src/components/DailyMatches.js
+++ bc-frontend/src/components/DailyMatches.js
@@ -6,17 +6,17 @@ function DailyMatches() {
         <h3 className='comp-title'>Today's Compatibility</h3>
     <div className='daily-matches'>
      <div className='daily-match'>
-       <img src="https://thispersondoesnotexist.com/image" alt="match1" className='dm-img'/>
+       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj5olWjirTVdEJTss1QfCE3JKF77gYE3C39A&usqp=CAU" alt="match1" className='dm-img'/>
        <p className='dm-bio'>Michael, 35 - Aries<br/>New York, NY <br/>Likes: Hiking
        </p>
     </div>
     <div className='daily-match'>
-       <img src="https://thispersondoesnotexist.com/image" alt="match2" className='dm-img' />
+       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTk4VzPbKKwMt2Nro3ODgYDZm3xJB96EYzhw&usqp=CAU" alt="match2" className='dm-img' />
        <p className='dm-bio'>Julia, 23 - Gemini<br/>Toronto, CA <br/>Likes: Art, Dogs
        </p>
     </div>
     <div className='daily-match'>
-       <img src="https://thispersondoesnotexist.com/image" alt="match3" className='dm-img'/>
+       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfH0VoIrtU9JMEDiinAHHlnv-eqKnEnHZvnw&usqp=CAU" alt="match3" className='dm-img'/>
        <p className='dm-bio'>Sam, 32 - Sagittarius<br/>Atlanta, GA <br/>Likes: Food, Sports
        </p>
     </div>
